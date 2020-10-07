54c9c7d833bf7d4082dbbc88eb7e02540fa45124
DmitriyDobrovolskiy <dda.90@mail.ru>
2020-10-07 08:14:12 +0500

    Created IObserver

---
+++ ObserverDesignPattern/ObserverDesignPattern/Observer/Observer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPattern.Observer
+{
+    public class Observer : IObserver<string>
+    {
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnNext(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
